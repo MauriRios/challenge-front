16d2af1f9141509d5c9c5f1b8ca91e077c448b19
MauriRios <87584747+MauriRios@users.noreply.github.com>
2023-04-08 14:27:54 -0300

    add: estilo a dialog de buscar venta por fecha, y lista de ventas en provider-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
 
-//components
-import { NavigationComponent } from './views/admin/navigation.component';
 
 //angular material
-import { AngularMaterialModule } from './shared/angular-material/angular-material.module';
-import { HttpClientModule } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { PurchasesDialogComponent } from './views/admin/customer-table/purchases-dialog/purchases-dialog.component';
-import { CustomerTableComponent } from './views/admin/customer-table/customer-table.component';
-import { ProviderTableComponent } from './views/admin/provider-table/provider-table.component';
 import { ViewsModule } from './views/views.module';
 import { DatePipe } from '@angular/common';
 
--- src/app/views/admin/sale-table/sale-by-date/sale-by-date.component.ts
+++ src/app/views/admin/sale-table/sale-by-date/sale-by-date.component.ts
@@ -1,7 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { SaleDTO } from 'src/app/models/saleDTO.model';
+import { Sort } from '@angular/material/sort';
+import { ProductSaleDTO } from 'src/app/models/productSaleDTO.model';
 import { SaleDataService } from 'src/app/services/sale-data.service';
 
 @Component({
@@ -11,7 +12,8 @@ import { SaleDataService } from 'src/app/services/sale-data.service';
 })
 export class SaleByDateComponent implements OnInit {
 
-  sales: SaleDTO[];
+  sales: any;
+  sortedData: ProductSaleDTO[];
 
   constructor(
     private saleDataService: SaleDataService,
@@ -33,4 +35,32 @@ export class SaleByDateComponent implements OnInit {
       
   }
 
+  sortData(sort: Sort) {
+    const data = this.sales.products.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id':
+          return compare(a.id, b.id, isAsc);
+          case 'name':
+            return compare(a.name, b.name, isAsc);
+        case 'description':
+          return compare(a.description, b.description, isAsc);
+        case 'price':
+          return compare(a.price, b.price, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
